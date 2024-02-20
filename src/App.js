bb7e7c2c2bb024579b6db5e3550a49fd2266c502
Raihan Zakariya <raihanzakariya49@gmail.com>
2024-02-20 20:36:12 +0530

    UseState02

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const state = useState();
-
-  const [count, setCount] = useState(0);
-  const interCount = () => {
-    setCount(count + 1);
+  let newTime = new Date().toLocaleTimeString();
+  let [actualTime, setTime] = useState(newTime);
+  const UpDate = () => {
+    let newCTime = new Date().toLocaleTimeString();
+    setTime(newCTime);
   };
-
   //  const count = useState();
   return (
     <>
-      <h1> {count} </h1>
-      <button onClick={interCount}> Click Me </button>
+      <div>
+        <h1 className="colorr"> {actualTime} </h1>
+        <button onClick={UpDate}> Get TIME </button>
+      </div>
     </>
   );
 };
