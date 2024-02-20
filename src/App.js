b0a5f4361ca746864d6bd4816624e44ef0a9a3ae
Raihan Zakariya <raihanzakariya49@gmail.com>
2024-02-20 19:30:53 +0530

    Hooks(UseState)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const state = useState();
+
+  const [count, setCount] = useState(0);
+  const interCount = () => {
+    setCount(count + 1);
+  };
+
+  //  const count = useState();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1> {count} </h1>
+      <button onClick={interCount}> Click Me </button>
+    </>
   );
-}
+};
 
 export default App;
